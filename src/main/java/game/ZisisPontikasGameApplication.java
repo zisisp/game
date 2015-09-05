11b62609061c4079240c8862da603db7a9dcb645
zisisp <zisis.p@gmail.com>
2015-09-05 21:30:50 +0300

    Initial commit

---
+++ src/main/java/game/ZisisPontikasGameApplication.java
@@ -0,0 +1,12 @@
+package game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZisisPontikasGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZisisPontikasGameApplication.class, args);
+    }
+}
