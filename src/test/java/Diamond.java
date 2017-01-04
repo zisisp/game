9685bb43a5ba693b7e439d4751fb8f06c591486d
zpontikas <z.pontikas@xe.gr>
2017-01-04 18:12:03 +0200

    Test8

--- src/test/java/Diamond.java
+++ src/test/java/Diamond.java
@@ -2,8 +2,41 @@
  * Created with IntelliJ IDEA.
  * User: zep
  * Date: 4/1/2017
- * Time: 1:18 μμ
+ * Time: 12:14 μμ
  * Company: www.xe.gr
  */
-public class Diamond {
+class Diamond {
+    public static String print(int n) {
+        if (n < 0 || n % 2 == 0)
+            return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(getSpaces(n, i));
+            sb.append(getDiamonds(n, i));
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    private static String getDiamonds(int n, int i) {
+        int diamonds;
+        if (i <= n / 2) {
+            diamonds = 1 + i * 2;
+        } else {
+            diamonds=n-((i-n/2)*2);
+        }
+        StringBuilder sb=new StringBuilder();
+        for (int j = 0; j < diamonds; j++) {
+            sb.append("*");
+        }
+        return sb.toString();
+    }
+
+    private static String getSpaces(int n, int i) {
+        StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < Math.abs(n/2-i); j++) {
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
 }
--- src/test/java/DiamondTest.java
+++ src/test/java/DiamondTest.java
@@ -1,11 +1,41 @@
-package PACKAGE_NAME;
+import org.junit.Test;
 
-/**
- * Created with IntelliJ IDEA.
- * User: zep
- * Date: 4/1/2017
- * Time: 1:19 μμ
- * Company: www.xe.gr
- */
+import static org.junit.Assert.assertEquals;
+
 public class DiamondTest {
+    @Test
+    public void testDiamond3() {
+        StringBuffer expected = new StringBuffer();
+        expected.append(" *\n");
+        expected.append("***\n");
+        expected.append(" *\n");
+        System.out.println("Diamond.print3 = \n" + Diamond.print(3));
+        assertEquals(expected.toString(), Diamond.print(3));
+    }
+
+    @Test
+    public void testDiamond5() {
+        StringBuffer expected = new StringBuffer();
+        expected.append("  *\n");
+        expected.append(" ***\n");
+        expected.append("*****\n");
+        expected.append(" ***\n");
+        expected.append("  *\n");
+        System.out.println("Diamond.print5 = \n" + Diamond.print(5));
+        assertEquals(expected.toString(), Diamond.print(5));
+    }
+
+    @Test
+    public void testDiamond7() {
+        StringBuffer expected = new StringBuffer();
+        expected.append("   *\n");
+        expected.append("  ***\n");
+        expected.append(" *****\n");
+        expected.append("*******\n");
+        expected.append(" *****\n");
+        expected.append("  ***\n");
+        expected.append("   *\n");
+        System.out.println("Diamond.print7 = \n" + Diamond.print(7));
+        assertEquals(expected.toString(), Diamond.print(7));
+    }
 }
