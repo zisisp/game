0999448c52c39d1e4e1572c0631d1dc9dfdb2b40
zpontikas <z.pontikas@xe.gr>
2017-01-04 18:11:56 +0200

    Merge branch 'master' of C:\Users\zep\IdeaProjects\game with conflicts.

---
+++ src/test/java/Diamond.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: zep
+ * Date: 4/1/2017
+ * Time: 1:18 μμ
+ * Company: www.xe.gr
+ */
+public class Diamond {
+}
---
+++ src/test/java/DiamondTest.java
@@ -0,0 +1,11 @@
+package PACKAGE_NAME;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zep
+ * Date: 4/1/2017
+ * Time: 1:19 μμ
+ * Company: www.xe.gr
+ */
+public class DiamondTest {
+}
